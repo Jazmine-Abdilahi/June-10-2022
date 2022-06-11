e1a22cf00b7be5ad757d66b631fa83389e07dc9d
Jazmine Abdilahi <66798977+Jazmine-Abdilahi@users.noreply.github.com>
2022-06-10 22:38:56 -0500

    Add files via upload

---
+++ Closures.js
@@ -0,0 +1,17 @@
+const secureBooking = function() {
+    let passengerCount = 0;
+
+    return function() {
+        pasengerCount++;
+    }
+}
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+/* tThe rest are notes, And Abo im sorry this is late and the intetion i did so little is cause Jabril is excessively behinde me. Consequently recognizing his unfavorable mistake, He told me to do only a little bit of stuff. I hope you acknowledge our miscalculation and have some sypathy for us. Have a pleasant remainder of your nigh */
